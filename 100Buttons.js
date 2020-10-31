22f41b03a38a598e0c9f603eacbbf57128d5154b
GWilson444 <gregorywilsonlmt@gmail.com>
2020-10-31 03:07:56 -0700

    initial commit

---
+++ 100Buttons.js
@@ -0,0 +1,6 @@
+// WRITE YOUR CODE IN HERE:
+for(let i = 1; i <= 100; i++){
+    const newButton = document.createElement('button');
+    newButton.innerText = 'Buttons!';
+    document.getElementById('container').appendChild(newButton);
+}
